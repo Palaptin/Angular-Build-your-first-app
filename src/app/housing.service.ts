f764e1a98d951e56bbec0954de0e7d234f4303ee
Palaptin <>
2024-01-09 14:03:44 +0100

    added an form, to enable user input (and log it)

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -7,7 +7,7 @@ import { HousingLocation } from './housinglocation';
 })
 export class HousingService {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  
+
   housingLocationList: HousingLocation[] = [
     {
       id: 0,
@@ -120,4 +120,8 @@ export class HousingService {
   }
 
   constructor() { }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
 }
